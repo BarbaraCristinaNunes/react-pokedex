ab89481e73f2c30a22d96ef0219befba6d5e6fc1
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-07-18 14:46:39 +0200

    Create PokemonCard.js

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+
+export default function PokemonCard() {
+    return (
+      <Card sx={{ maxWidth: 200 }}>
+        <CardMedia
+          component="img"
+          alt="green iguana"
+          height="auto"
+          image='/pokeball.png'
+        >
+        </CardMedia>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Name
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+            <Grid 
+                container 
+                spacing={1}
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+                <Grid item lg={6} xs={6}>
+                    <Button size="small">Evolution</Button>
+                </Grid>
+                <Grid item lg={6} xs={6} style={{textAlign: "right"}}>
+                    <Button size="small">Type</Button>
+                </Grid>            
+            </Grid>
+
+        </CardActions>
+      </Card>
+    );
+  }
