53ded2a51f82c58ec5ed9c2ad80ec2c147c2d2e6
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-07-20 14:56:19 +0200

    Update PokemonBox.js

--- src/components/PokemonBox.js
+++ src/components/PokemonBox.js
@@ -1,70 +1,118 @@
 import * as React from 'react';
-import { Box, Grid, Paper } from '@mui/material';
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import PokemonCard from './PokemonCard';
-import Pokemon from './Pokemon';
-export default function PokemonBox() {
+import {getPokemonByInput} from '../crud/crud';
+export default function PokemonBox(props) {
+    // console.log("from box ", props.pokemon)
     return (
-        <ThemeProvider theme={myTheme}>
-            <Box
-                sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                    m: 1,
-                    width: "90%",
-                    height: 128,
-                },
-                }}
-                style={{justifyContent: "center"}}
-            >
-                <Paper 
-                    elevation={24} 
-                    style={{
-                        maxWidth: "90%",
-                        marginTop: 50,
-                        height: "auto"
+        <>
+            <ThemeProvider theme={myTheme}>
+                <Box
+                    sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    '& > :not(style)': {
+                        m: 1,
+                        width: "90%",
+                        height: 128,
+                    },
                     }}
+                    style={{justifyContent: "center"}}
                 >
-                    <Grid
-                        container 
-                        spacing={1}
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center"
-                        style={{margin: 40}}
+                    <Paper 
+                        elevation={24} 
+                        style={{
+                            maxWidth: "90%",
+                            marginTop: 50,
+                            height: "auto"
+                        }}
                     >
-                        <Grid 
-                            item 
-                            lg={12} 
-                            xs={12} 
-                            style={{ textAlign: "center", marginBottom: 50}}
+                        <Grid
+                            container 
+                            spacing={1}
+                            direction="row"
+                            alignItems="center"
+                            justifyContent="center"
+                            style={{margin: 40}}
                         >
-                            <FormControl sx={{ m: 1, minWidth: 120 }} color="third" focused>
-                                <InputLabel htmlFor="grouped-native-select">Select by</InputLabel>
-                                <Select native defaultValue="" id="grouped-native-select" label="Select by">
-                                    <option aria-label="None" value="" />
-                                    <option value={3}>
-                                        Favorite
-                                    </option>
-                                    <optgroup label="Type">
-                                        <option value={1}>Option 1</option>
-                                        <option value={2}>Option 2</option>
-                                    </optgroup>                        
-                                </Select>
-                            </FormControl>
+                            <Grid 
+                                item 
+                                lg={12} 
+                                xs={12} 
+                                style={{ textAlign: "center", marginBottom: 50}}
+                            >
+                                <FormControl sx={{ m: 1, minWidth: 120 }} color="third" focused>
+                                    <InputLabel htmlFor="grouped-native-select">Select by</InputLabel>
+                                    <Select native defaultValue="" id="grouped-native-select" label="Select by">
+                                        <option aria-label="None" value="" />
+                                        <option value={3}>
+                                            Favorite
+                                        </option>
+                                        <optgroup label="Type">
+                                            <option value={1}>Option 1</option>
+                                            <option value={2}>Option 2</option>
+                                        </optgroup>                        
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                            {props.pokemon !== undefined ?
+                                <>
+                                    {
+                                        props.pokemon.map((pokemon, index) => {
+                                            return (
+                                                <Grid 
+                                                    item 
+                                                    key={`${index}Card`} 
+                                                    lg={3} 
+                                                    md={6} 
+                                                    xs={12} 
+                                                    style={{marginBottom: 50}}
+                                                >
+                                                    <PokemonCard pokemon={pokemon}/>
+                                                </Grid>
+                                            )
+                                        })
+                                    }
+                                    {
+                                        props.pokemon.length > 1 ?
+                                            <Grid item lg={12} xs={12} style={{textAlign: "center"}}> 
+                                                <Button 
+                                                    variant="contained" 
+                                                    color="primary"
+                                                    onClick={(v) => {
+                                                        console.log("pokeLength1 ", props.pokeLength);
+                                                        let sum = props.pokeLength + 8;
+                                                        let arr = props.pokemon;
+                                                        console.log("sum ", sum, "arr ", arr)
+                                                        for(let i = props.pokeLength; i < sum; i++){
+                                                            getPokemonByInput(i)
+                                                                .then((data) => {
+                                                                    arr.push(data);
+                                                                })
+                                                        }
+                                                        props.setAllPokemons(arr);
+                                                        props.setPokeLength(sum);
+                                                        console.log("props.pokeLength ", props.pokeLength, "props.pokemon ", props.pokemon)
+                                                    }}
+                                                >
+                                                    Show more
+                                                </Button>
+                                            </Grid>
+                                        : ""
+                                    }
+                                    
+                                </>
+                                : ""
+                            }
                         </Grid>
-                        <Grid item lg={3} md={6} xs={12} style={{marginBottom: 50}}>
-                            {/* <Pokemon/> */}
-                            <PokemonCard/>
-                        </Grid>
-                    </Grid>
-                </Paper>
-            </Box>
-        </ThemeProvider>
+                    </Paper>
+                </Box>
+            </ThemeProvider>
+        </>
     );
 }
 
