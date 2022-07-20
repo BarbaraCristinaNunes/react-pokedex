18478ba2e077501a5fc0bc56f382d3931456b7bd
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-07-20 14:56:23 +0200

    Update crud.js

--- src/crud/crud.js
+++ src/crud/crud.js
@@ -1,11 +1,30 @@
 import axios from 'axios';
-export async function getPokemon(data) {
+export async function getPokemonByInput(data) {
     const url = `https://pokeapi.co/api/v2/pokemon/${data}`;
     try {
         const res = await axios.get(url);
-        console.log("from crud ", res.data);
         return res.data;
     } catch (e) {
         return e;
     }
 }
+
+export async function getPokemonByUrl(data) {
+    const url = data;
+    try {
+        const res = await axios.get(url);
+        return res.data;
+    } catch (e) {
+        return e;
+    }
+}
+
+// export async function getAllPokemon(data) {
+//     const url = "https://pokeapi.co/api/v2/pokemon?limit=811";
+//     try {
+//         const res = await axios.get(url);
+//         return res.data;
+//     } catch (e) {
+//         return e;
+//     }
+// }
