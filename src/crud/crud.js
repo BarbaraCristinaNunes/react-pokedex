e30e7f74892a159a74f76864185b570cb11f38f2
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2022-07-28 14:06:36 +0200

    created code to get evolutions from pokemons

--- src/crud/crud.js
+++ src/crud/crud.js
@@ -3,6 +3,7 @@ export async function getPokemonByInput(data) {
     const url = `https://pokeapi.co/api/v2/pokemon/${data}`;
     try {
         const res = await axios.get(url);
+        // console.log("res.data ", res.data)
         return res.data;
     } catch (e) {
         return e;
@@ -19,12 +20,15 @@ export async function getPokemonByUrl(data) {
     }
 }
 
-// export async function getAllPokemon(data) {
-//     const url = "https://pokeapi.co/api/v2/pokemon?limit=811";
-//     try {
-//         const res = await axios.get(url);
-//         return res.data;
-//     } catch (e) {
-//         return e;
-//     }
-// }
+export async function getPokemonEvolution(data) {
+    const url = `https://pokeapi.co/api/v2/pokemon-species/${data}`;
+    try {
+        const res = await axios.get(url);
+        const evolutionsUrl = res.data.evolution_chain.url;
+        const data = await axios.get(evolutionsUrl);
+        // console.log("res.data ", data)
+        return data.data.chain;
+    } catch (e) {
+        return e;
+    }
+}
